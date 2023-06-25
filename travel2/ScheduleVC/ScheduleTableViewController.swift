8144b96d162ba26c3cff997dde1a2a41e3993623
Shang-JungWu <shangjungw@gmail.com>
2023-06-25 21:32:04 +0800

    新增儲存行程、長按 delete all 功能

--- travel2/ScheduleVC/ScheduleTableViewController.swift
+++ travel2/ScheduleVC/ScheduleTableViewController.swift
@@ -9,41 +9,83 @@ import UIKit
 import UniformTypeIdentifiers
 
 class ScheduleTableViewController: UITableViewController {
-    
-    lazy var schedules = [Schedule]() // 大ㄉ
-    lazy var userSchedules = [userSchedule]() // 小ㄉ
-    
-    
+
+    lazy var schedules = [Schedule]() // 包含Date的
+    lazy var userSchedules = [userSchedule]() // 每日細項
+
     var addScheduleButton: UIButton!
     var addButtonTag = 0
     let datePickerInAlertSheet = UIDatePicker()
     var sectionDatePickerTag = 0
     let formatter = DateFormatter()
-    
-    //MARK: - Life Cycle
+
+
+    //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // 代理人
         tableView.dragInteractionEnabled = true
         tableView.dragDelegate = self
         tableView.dropDelegate = self
-      
+
         // Nav
         navigationController?.navigationBar.isHidden = false
         navigationController?.navigationBar.backgroundColor = UIColor(red: 44/255, green: 54/255, blue: 57/255, alpha: 1)
 
-        // Nav title
-        let titleLabel = UILabel()
-        titleLabel.text = "我的行程"
-        titleLabel.textColor = UIColor.white
-        titleLabel.sizeToFit()
-        navigationItem.titleView = titleLabel
+        // 設定 Navigation Bar
+        navigationItem.title = "我的行程"
+        navigationController?.navigationBar.titleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.white]
+
         // Nav rightBarButton
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "calendar.badge.plus"), style: .plain, target: self, action: #selector(addDate))
         navigationItem.rightBarButtonItem?.tintColor = .white
-        
-       
+
+        // Nav lefttBarButton
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveSchedule))
+        navigationItem.leftBarButtonItem?.tintColor = .white
+
+        if let data = UserDefaults.standard.data(forKey: "userSchedule") {
+            schedules = try! JSONDecoder().decode([Schedule].self, from: data)
+        }
+
+        // 長按刪除
+        let longPress = UILongPressGestureRecognizer(
+          target: self,
+          action: #selector(longPress))
+
+        // 加入監聽手勢
+        self.view.addGestureRecognizer(longPress)
+
+    }
+
+    @objc func longPress(recognizer: UILongPressGestureRecognizer) {
+        recognizer.minimumPressDuration = 2.5
+        if recognizer.state == .began {
+            let alert = UIAlertController(title: "是否刪除所有行程？", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "全部刪除", style: .destructive, handler: { [self]_ in
+                schedules.removeAll()
+                UserDefaults.standard.removeObject(forKey: "userSchedule")
+                tableView.reloadData()
+            }))
+            alert.addAction(UIAlertAction(title: "取消", style: .default))
+            present(alert, animated: true)
+            print("長按開始")
+        } else if recognizer.state == .ended {
+            print("長按結束")
+        }
+
+    }
+
+    @objc func saveSchedule(){
+        let data = try? JSONEncoder().encode(schedules)
+        if let data = data {
+            UserDefaults.standard.setValue(data, forKey: "userSchedule")
+        }
+        let alert = UIAlertController(title: "行程已儲存", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+
     }
 
     @objc func addDate() {
@@ -51,29 +93,32 @@ class ScheduleTableViewController: UITableViewController {
         datePickerInAlertSheet.datePickerMode = .date
         let alert = UIAlertController(title: "Choose a Date", message: nil, preferredStyle: .actionSheet)
         alert.view.addSubview(datePickerInAlertSheet)
-        
-        // 定位
+
+        // date picker 定位
         datePickerInAlertSheet.translatesAutoresizingMaskIntoConstraints = false
         datePickerInAlertSheet.topAnchor.constraint(equalTo: alert.view.topAnchor, constant: alert.view.frame.height * 0.05).isActive = true
         datePickerInAlertSheet.centerXAnchor.constraint(equalTo: alert.view.centerXAnchor).isActive = true
         datePickerInAlertSheet.widthAnchor.constraint(equalToConstant: alert.view.frame.width * 0.85).isActive = true
 
-        let okAction = UIAlertAction(title: "Add", style: .default) { _ in
+        let okAction = UIAlertAction(title: "新增", style: .default) { _ in
             self.selectDate()
             self.tableView.reloadData()
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel)
         alert.addAction(okAction)
         alert.addAction(cancelAction)
 
         let height: NSLayoutConstraint = NSLayoutConstraint(item: alert.view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: self.view.frame.height * 0.57)
         alert.view.addConstraint(height)
         // https://stackoverflow.com/questions/25780599/add-datepicker-in-uiactionsheet-using-swift
+        // for ipad only (crash without it)，同時必須註解掉 date picker 定位
+//        let popOverController = alert.popoverPresentationController
+//        popOverController!.barButtonItem = navigationItem.rightBarButtonItem
 
             self.present(alert, animated: true)
-   
+
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "scheduleShowDetail" {
             let detailVC = segue.destination as! DetailViewController
@@ -84,7 +129,15 @@ class ScheduleTableViewController: UITableViewController {
             detailVC.segueID = segue.identifier!
         }
     }
-    
+
+    override func viewWillDisappear(_ animated: Bool) {
+        print("消失")
+        let data = try? JSONEncoder().encode(schedules)
+        if let data = data {
+            UserDefaults.standard.setValue(data, forKey: "userSchedule")
+        }
+
+    }
 
     //MARK: - 自訂函式
     // 選擇日期
@@ -95,7 +148,7 @@ class ScheduleTableViewController: UITableViewController {
         schedules.append(Schedule(date: date))
         self.tableView.reloadData()
     }
-    
+
     // 更改日期 value change
     @IBAction func dateChange(_ sender: UIDatePicker) {
         formatter.dateFormat = "yyyyMMdd"
@@ -103,13 +156,7 @@ class ScheduleTableViewController: UITableViewController {
         print(schedules[sender.tag])
         self.tableView.reloadData()
     }
-    
-    @IBAction func datePickerTapped(_ sender: UIDatePicker) {
 
-            print("press date picker")
-    }
-    
-    
     // 更改時間 value change
     @IBAction func timeChange(_ sender: UIDatePicker) {
         let point = sender.convert(CGPoint.zero, to: tableView)
@@ -122,21 +169,19 @@ class ScheduleTableViewController: UITableViewController {
     }
 
 
-
-    // 呼叫 bottom sheet
+    // 呼叫 bottom sheets
     func presentCollectionSheet() {
         let collectionController = storyboard?.instantiateViewController(identifier: "CollectionSheetVC") as! CollectionTableViewController
         let navController = UINavigationController(rootViewController: collectionController)
         if let sheetPresentationController = navController.sheetPresentationController {
             sheetPresentationController.prefersGrabberVisible = true
             sheetPresentationController.largestUndimmedDetentIdentifier = .medium
-            sheetPresentationController.detents = [.medium(), .large()]
-            sheetPresentationController .preferredCornerRadius = 25
-            }
+            sheetPresentationController.detents = [.medium()]
+            sheetPresentationController.preferredCornerRadius = 25
+        }
         self.present(navController, animated: true, completion: nil)
-        
     }
-    
+
 
     func presentCustomScheduleSheet() {
         let customScheduleController = storyboard?.instantiateViewController(withIdentifier: "CustomSheetViewController") as! CustomSheetViewController
@@ -149,9 +194,9 @@ class ScheduleTableViewController: UITableViewController {
         sheetPresentationController?.preferredCornerRadius = 25
         self.present(navigationController, animated: true)
     }
-    
-   
-    
+
+
+
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         return schedules.count
@@ -160,7 +205,7 @@ class ScheduleTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return  schedules[section].schedule.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ScheduleTableViewCell", for: indexPath) as! ScheduleTableViewCell
         cell.scheduleLabel.text = schedules[indexPath.section].schedule[indexPath.row].placeName.name
@@ -171,7 +216,7 @@ class ScheduleTableViewController: UITableViewController {
         cell.backgroundColor = UIColor(red: 240/255, green: 235/255, blue: 227/255, alpha: 1)
         return cell
     }
-    
+
     // section header
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
        return 50
@@ -191,13 +236,21 @@ class ScheduleTableViewController: UITableViewController {
 
         addScheduleButton = UIButton(type: .system)
         addScheduleButton.tag = section
-        addScheduleButton.frame = CGRect(x: cell.contentView.frame.maxX - 50 - cell.contentView.frame.width * 0.1, y: 0, width: 50, height: 50)
-       
+        addScheduleButton.frame = CGRect(x: self.view.frame.maxX - 50, y: 0, width: 50, height: 50)
         addScheduleButton.setImage(UIImage(systemName: "line.horizontal.3"), for: .normal)
         addScheduleButton.tintColor = .white
         addScheduleButton.showsMenuAsPrimaryAction = true
         addScheduleButton.menu = UIMenu(children: [
             UIAction(title: "我的收藏", handler: { [self] action in
+
+                let headerFrameY = tableView.rectForHeader(inSection: section).maxY
+                if headerFrameY >= self.view.frame.height * 0.5 {
+
+                    self.view.frame.origin.y = -(self.view.frame.height * 0.5)
+                } else {
+                    self.view.frame.origin.y = 0
+                }
+
                 presentCollectionSheet()
             }),
             UIAction(title: "新增自訂目的地", handler: { [self] action in
@@ -210,6 +263,10 @@ class ScheduleTableViewController: UITableViewController {
                 alert.addAction(UIAlertAction(title: "取消", style: .cancel))
                 alert.addAction(UIAlertAction(title: "確定", style: .destructive, handler: { [self] action in
                     schedules.remove(at: section)
+                    let data = try? JSONEncoder().encode(schedules)
+                    if let data = data {
+                        UserDefaults.standard.setValue(data, forKey: "userSchedule")
+                    }
                     tableView.reloadData()
                 }))
                 self.present(alert, animated: true)
@@ -219,30 +276,32 @@ class ScheduleTableViewController: UITableViewController {
         return cell.contentView
     }
 
-    
+
     // footer
     override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let footerView = UIView()
         footerView.backgroundColor = UIColor(red: 240/255, green: 235/255, blue: 227/255, alpha: 1)
-        
+
         return footerView
     }
-    
+
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         20
     }
-    
+
     // 滑動刪除
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         schedules[indexPath.section].schedule.remove(at: indexPath.row)
+        let data = try? JSONEncoder().encode(schedules)
+        if let data = data {
+            UserDefaults.standard.setValue(data, forKey: "userSchedule")
+        }
         tableView.deleteRows(at: [indexPath], with: .automatic)
     }
-    
+
     override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
         "刪除"
     }
-
-
 
 
 } // class ending
@@ -260,21 +319,19 @@ extension ScheduleTableViewController: UITableViewDragDelegate, UITableViewDropD
         return [dragItem]
 
     }
-    
+
     func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
         if coordinator.session.hasItemsConforming(toTypeIdentifiers: [UTType.plainText.identifier as String]){
             coordinator.session.loadObjects(ofClass: NSString.self) { items in
-                
+
                 guard let string = items.first as? String else { return }
-                print("string:", string)
                 switch (coordinator.items.first?.sourceIndexPath, coordinator.destinationIndexPath) {
                 //－－－－－－讓 cell 在 Schedule Table View 的任意 section 移動－－－－－－
                 case (.some(let sourceIndexPath), .some(let destinationIndexPath)):
                     if sourceIndexPath.section == destinationIndexPath.section {
                         let tmp = self.schedules[sourceIndexPath.section].schedule.remove(at: sourceIndexPath.row)
-                        
+
                         self.schedules[sourceIndexPath.section].schedule.insert(tmp, at: destinationIndexPath.row)
-                        print(tmp)
                         self.tableView.reloadData()
                     } else {
                         let tmp = self.schedules[sourceIndexPath.section].schedule.remove(at: sourceIndexPath.row)
@@ -284,28 +341,25 @@ extension ScheduleTableViewController: UITableViewDragDelegate, UITableViewDropD
                     }
                     self.tableView.performBatchUpdates(nil)
                     break
-                    
+
                 //－－－－－－從sheet移動項目到底面表格－－－－－－
                 case (nil, .some(let destinationIndexPath)):
 
                     self.schedules[destinationIndexPath.section].schedule.insert(userSchedule(placeName: collectionItem), at: destinationIndexPath.row)
-                    
-//                    self.schedules[destinationIndexPath.section].schedule.insert(userSchedule(placeName: TainanPlaces(name: string, openTime: "", district: "", address: string, tel: "", lat: nil, long: nil)), at: destinationIndexPath.row)
-
                     self.tableView.insertRows(at: [destinationIndexPath], with: .automatic)
                     self.tableView.performBatchUpdates(nil)
                     print("從sheet移動:",self.schedules)
                     break
-                    
+
                 default: break
-                    
+
                 }
             }
         }
     }
-    
+
     func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
         return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
     }
-    
+
 }
